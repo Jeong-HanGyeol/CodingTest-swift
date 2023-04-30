face76fc432a74890d3b22daba01f34a145e1c94
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-01 00:30:46 +0900

    백준 1001번

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,7 +1,7 @@
-// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+// 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
 // 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
-// 첫째 줄에 A+B를 출력한다.
-// 예제 입력 1 2
+// 첫째 줄에 A-B를 출력한다.
+// 예제 입력 3 2
 
 import Foundation
 
@@ -9,4 +9,4 @@ let input = readLine()!
 let inputArr = input.components(separatedBy: " ")
 let a = Int(inputArr[0])!
 let b = Int(inputArr[1])!
-print(a+b)
+print(a-b)
