02256903ae3c4a400f31a050521a14d9f5c16c1e
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-01 01:08:47 +0900

    백준 10869번

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,10 +1,14 @@
-//두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
-//첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
-//첫째 줄에 A/B를 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-9 이하이면 정답이다.
-//예제입력 1 3
+//두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.
+//두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000)
+//첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+//예제입력 7 3
 import Foundation
 let input = readLine()!
 let inputArr = input.components(separatedBy: " ")
-let a = Double(inputArr[0])!
-let b = Double(inputArr[1])!
+let a = Int(inputArr[0])!
+let b = Int(inputArr[1])!
+print(a+b)
+print(a-b)
+print(a*b)
 print(a/b)
+print(a%b)
