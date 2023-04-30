1de8e615a690dbc4b4e53c4933fb3bd286455658
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-01 00:53:42 +0900

    백준 1008번

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,11 +1,10 @@
-//두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+//두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
 //첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
-//첫째 줄에 A×B를 출력한다.
-//예제 입력 1 2
-
+//첫째 줄에 A/B를 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-9 이하이면 정답이다.
+//예제입력 1 3
 import Foundation
 let input = readLine()!
 let inputArr = input.components(separatedBy: " ")
-let a = Int(inputArr[0])!
-let b = Int(inputArr[1])!
-print(a*b)
+let a = Double(inputArr[0])!
+let b = Double(inputArr[1])!
+print(a/b)
