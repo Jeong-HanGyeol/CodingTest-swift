b0b42fecbc631db364946cf014b5ef9a2acbcf15
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-01 00:47:07 +0900

    백준 10998번

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,12 +1,11 @@
-// 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
-// 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
-// 첫째 줄에 A-B를 출력한다.
-// 예제 입력 3 2
+//두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+//첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+//첫째 줄에 A×B를 출력한다.
+//예제 입력 1 2
 
 import Foundation
-
 let input = readLine()!
 let inputArr = input.components(separatedBy: " ")
 let a = Int(inputArr[0])!
 let b = Int(inputArr[1])!
-print(a-b)
+print(a*b)
