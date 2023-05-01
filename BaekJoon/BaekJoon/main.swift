ba80f5c2a484efcf8ac28e122c9d0626393ee28c
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-01 18:26:40 +0900

    백준 2588번

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,17 +1,21 @@
-// (A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
-//(A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
-//세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
-//첫째 줄에 A, B, C가 순서대로 주어진다. (2 ≤ A, B, C ≤ 10000)
-//첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
-//예제입력 5 8 4
+//    (세 자리 수) × (세 자리 수)는 다음과 같은 과정을 통하여 이루어진다.
+//    (1)과 (2)위치에 들어갈 세 자리 자연수가 주어질 때 (3), (4), (5), (6)위치에 들어갈 값을 구하는 프로그램을 작성하시오.
+//첫째 줄에 (1)의 위치에 들어갈 세 자리 자연수가, 둘째 줄에 (2)의 위치에 들어갈 세자리 자연수가 주어진다.
+//첫째 줄부터 넷째 줄까지 차례대로 (3), (4), (5), (6)에 들어갈 값을 출력한다.
+//예제입력
+//472
+//385
 
 import Foundation
-let input = readLine()!
-let inputArr = input.components(separatedBy: " ")
-let a = Int(inputArr[0])!
-let b = Int(inputArr[1])!
-let c = Int(inputArr[2])!
-print((a+b)%c)
-print(((a%c)+(b%c))%c)
-print((a*b)%c)
-print(((a%c)*(b%c))%c)
+let input1 = readLine()!
+let input2 = readLine()!
+let input1_num = Int(input1)!
+let input2_num = Int(input2)!
+let input2_1 = input2_num % 10
+let input2_2 = (input2_num/10) % 10
+let input2_3 = (input2_num/100) % 10
+
+print(input1_num*input2_1)
+print(input1_num*input2_2)
+print(input1_num*input2_3)
+print(input1_num*input2_num)
