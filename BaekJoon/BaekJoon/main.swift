21fd0eb9584f27f96d7e9cd0e10abbcbf6e0b00a
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-01 18:33:56 +0900

    백준 11382번

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,21 +1,12 @@
-//    (세 자리 수) × (세 자리 수)는 다음과 같은 과정을 통하여 이루어진다.
-//    (1)과 (2)위치에 들어갈 세 자리 자연수가 주어질 때 (3), (4), (5), (6)위치에 들어갈 값을 구하는 프로그램을 작성하시오.
-//첫째 줄에 (1)의 위치에 들어갈 세 자리 자연수가, 둘째 줄에 (2)의 위치에 들어갈 세자리 자연수가 주어진다.
-//첫째 줄부터 넷째 줄까지 차례대로 (3), (4), (5), (6)에 들어갈 값을 출력한다.
-//예제입력
-//472
-//385
+//꼬마 정민이는 이제 A + B 정도는 쉽게 계산할 수 있다. 이제 A + B + C를 계산할 차례이다!
+//첫 번째 줄에 A, B, C (1 ≤ A, B, C ≤ 1012)이 공백을 사이에 두고 주어진다.
+//A+B+C의 값을 출력한다.
+//예제입력 77 77 7777
 
 import Foundation
-let input1 = readLine()!
-let input2 = readLine()!
-let input1_num = Int(input1)!
-let input2_num = Int(input2)!
-let input2_1 = input2_num % 10
-let input2_2 = (input2_num/10) % 10
-let input2_3 = (input2_num/100) % 10
-
-print(input1_num*input2_1)
-print(input1_num*input2_2)
-print(input1_num*input2_3)
-print(input1_num*input2_num)
+let input = readLine()!
+let inputArr = input.components(separatedBy: " ")
+let a = Int(inputArr[0])!
+let b = Int(inputArr[1])!
+let c = Int(inputArr[2])!
+print(a+b+c)
