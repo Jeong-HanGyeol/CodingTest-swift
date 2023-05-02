cfb7916bbb0f7846bf59b2dd6d78fd2b7cf5fae0
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-02 13:40:29 +0900

    9498번

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,20 +1,19 @@
-//두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
-//첫째 줄에 A와 B가 주어진다. A와 B는 공백 한 칸으로 구분되어져 있다.
-//첫째 줄에 다음 세 가지 중 하나를 출력한다.
-//A가 B보다 큰 경우에는 '>'를 출력한다.
-//A가 B보다 작은 경우에는 '<'를 출력한다.
-//A와 B가 같은 경우에는 '=='를 출력한다.
-//예제입력 1 2
+//시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+//첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+//시험 성적을 출력한다.
+//예제입력 100
 
 import Foundation
 let input = readLine()!
-let inputArr = input.components(separatedBy: " ")
-let a = Int(inputArr[0])!
-let b = Int(inputArr[1])!
-if a > b {
-    print(">")
-} else if a < b {
-    print("<")
-} else if a == b {
-    print("==")
+let inputNum = Int(input)!
+if inputNum >= 90 {
+    print("A")
+} else if inputNum >= 80 && inputNum <= 89 {
+    print("B")
+} else if inputNum >= 70 && inputNum <= 79 {
+    print("C")
+} else if inputNum >= 60 && inputNum <= 69 {
+    print("D")
+} else {
+    print("F")
 }
