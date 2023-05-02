03eadbcacba527569148d7df4df9255de438d5bf
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-02 13:18:23 +0900

    백준 1330번

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,12 +1,20 @@
-//아래 예제와 같이 개를 출력하시오.
-//|\_/|
-//|q p|   /}
-//( 0 )"""\
-//|"^"`    |
-//||_/=\\__|
+//두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
+//첫째 줄에 A와 B가 주어진다. A와 B는 공백 한 칸으로 구분되어져 있다.
+//첫째 줄에 다음 세 가지 중 하나를 출력한다.
+//A가 B보다 큰 경우에는 '>'를 출력한다.
+//A가 B보다 작은 경우에는 '<'를 출력한다.
+//A와 B가 같은 경우에는 '=='를 출력한다.
+//예제입력 1 2
+
 import Foundation
-print(#"|\_/|"#)
-print(#"|q p|   /}"#)
-print(#"( 0 )"""\"#)
-print(#"|"^"`    |"#)
-print(#"||_/=\\__|"#)
+let input = readLine()!
+let inputArr = input.components(separatedBy: " ")
+let a = Int(inputArr[0])!
+let b = Int(inputArr[1])!
+if a > b {
+    print(">")
+} else if a < b {
+    print("<")
+} else if a == b {
+    print("==")
+}
