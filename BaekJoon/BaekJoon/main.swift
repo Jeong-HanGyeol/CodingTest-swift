0da783c655c6148730afe3a55ae7515e9995cd7e
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-03 10:45:40 +0900

    백준 10950번

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,11 +1,14 @@
-//N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오. 출력 형식에 맞춰서 출력하면 된다.
-//첫째 줄에 N이 주어진다. N은 1보다 크거나 같고, 9보다 작거나 같다.
-//출력형식과 같게 N*1부터 N*9까지 출력한다.
-//예제입력 2
+//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+//첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+//각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+//각 테스트 케이스마다 A+B를 출력한다.
 
 import Foundation
 let input = readLine()!
-let num = Int(input)!
-for i in 1...9 {
-    print("\(num) * \(i) = \(num*i)")
+let n = Int(input)!
+var int = 0
+while int < n {
+    let num = readLine()!.split(separator: " ").map{ Int($0)! }
+    int+=1
+    print(num[0] + num[1])
 }
