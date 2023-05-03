45a326eaa788c15bfb0a17e111d8ed92f571d625
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-03 11:18:43 +0900

    백준 25304번

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,12 +1,32 @@
-//n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
-//첫째 줄에 n (1 ≤ n ≤ 10,000)이 주어진다.
-//1부터 n까지 합을 출력한다.
-
+//준원이는 저번 주에 살면서 처음으로 코스트코를 가 봤다. 정말 멋졌다. 그런데, 몇 개 담지도 않았는데 수상하게 높은 금액이 나오는 것이다! 준원이는 영수증을 보면서 정확하게 계산된 것이 맞는지 확인해보려 한다.
+//영수증에 적힌,
+//구매한 각 물건의 가격과 개수
+//구매한 물건들의 총 금액
+//을 보고, 구매한 물건의 가격과 개수로 계산한 총 금액이 영수증에 적힌 총 금액과 일치하는지 검사해보자.
+//첫째 줄에는 영수증에 적힌 총 금액
+//$X$가 주어진다.
+//둘째 줄에는 영수증에 적힌 구매한 물건의 종류의 수
+//$N$이 주어진다.
+//이후
+//$N$개의 줄에는 각 물건의 가격
+//$a$와 개수
+//$b$가 공백을 사이에 두고 주어진다.
+//구매한 물건의 가격과 개수로 계산한 총 금액이 영수증에 적힌 총 금액과 일치하면 Yes를 출력한다. 일치하지 않는다면 No를 출력한다.
 import Foundation
-let input = readLine()!
-let n = Int(input)!
-var total = 0
-for i in 1...n {
-    total += i
+let input1 = readLine()!
+let input2 = readLine()!
+let total = Int(input1)!
+let kind = Int(input2)!
+var totalNum = 0
+var num = 0
+while num < kind {
+    num+=1
+    let n = readLine()!.split(separator: " ").map{Int($0)!}
+    totalNum = totalNum + n[0] * n[1]
 }
-print(total)
+if totalNum == total {
+    print("Yes")
+} else {
+    print("No")
+}
+
