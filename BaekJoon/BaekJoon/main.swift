f961c502bfe70946544db933ec8ab6c93067a014
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-06 01:18:33 +0900

    백준 10818번

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,16 +1,8 @@
-//정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
-//첫째 줄에 N과 X가 주어진다. (1 ≤ N, X ≤ 10,000)
-//둘째 줄에 수열 A를 이루는 정수 N개가 주어진다. 주어지는 정수는 모두 1보다 크거나 같고, 10,000보다 작거나 같은 정수이다.
-//X보다 작은 수를 입력받은 순서대로 공백으로 구분해 출력한다. X보다 작은 수는 적어도 하나 존재한다.
+//
+//N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+//첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다. 모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
+//첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
 
-let inputArr = readLine()!.split(separator: " ")
-let arr = readLine()!.split(separator: " ").map {Int($0)!}
-let n = Int(inputArr[0])!
-let x = Int(inputArr[1])!
-var result = ""
-for i in 0..<n {
-    if arr[i] < x {
-        result += String(arr[i])+" "
-    }
-}
-print(result)
+let n = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map {Int($0)!}.sorted()
+print("\(arr[0]) \(arr[n-1])")
