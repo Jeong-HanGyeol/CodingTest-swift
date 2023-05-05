6861a4ffc5b38edb216929566c4b1070eabac854
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-06 01:01:32 +0900

    백준 10871번' clear

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,14 +1,16 @@
+//정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
+//첫째 줄에 N과 X가 주어진다. (1 ≤ N, X ≤ 10,000)
+//둘째 줄에 수열 A를 이루는 정수 N개가 주어진다. 주어지는 정수는 모두 1보다 크거나 같고, 10,000보다 작거나 같은 정수이다.
+//X보다 작은 수를 입력받은 순서대로 공백으로 구분해 출력한다. X보다 작은 수는 적어도 하나 존재한다.
 
-//총 N개의 정수가 주어졌을 때, 정수 v가 몇 개인지 구하는 프로그램을 작성하시오.
-//첫째 줄에 정수의 개수 N(1 ≤ N ≤ 100)이 주어진다. 둘째 줄에는 정수가 공백으로 구분되어져있다. 셋째 줄에는 찾으려고 하는 정수 v가 주어진다. 입력으로 주어지는 정수와 v는 -100보다 크거나 같으며, 100보다 작거나 같다.
-//첫째 줄에 입력으로 주어진 N개의 정수 중에 v가 몇 개인지 출력한다.
-let n1 = Int(readLine()!)!
-let n2 = readLine()!.split(separator: " ").map {Int($0)!}
-let v = Int(readLine()!)!
-var result = 0
-for i in 0..<n1 {
-    if n2[i] == v {
-        result += 1
+let inputArr = readLine()!.split(separator: " ")
+let arr = readLine()!.split(separator: " ").map {Int($0)!}
+let n = Int(inputArr[0])!
+let x = Int(inputArr[1])!
+var result = ""
+for i in 0..<n {
+    if arr[i] < x {
+        result += String(arr[i])+" "
     }
 }
 print(result)
