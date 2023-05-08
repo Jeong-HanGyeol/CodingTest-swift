165eee393f9e5100a065ef35d7a73136e7b070b3
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-08 13:16:24 +0900

    백준 27866번

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,12 +1,5 @@
-
-let n = Int(readLine()!)!
-//var arr = Array<Double>()
-var m = readLine()!.split(separator: " ").map { Double($0)! }
-let max = m.max()!
-m = m.map { $0 / max }
-var mPlus:Double = 0.0
-
-for i in 0..<n {
-    mPlus += m[i]
-}
-print(mPlus / Double(n) * 100)
+let s = readLine()!
+let i = Int(readLine()!)!
+var cha = Array<String>()
+s.map {cha.append(String($0))}
+print(cha[i-1])
