cdd5a2ef7b21d4be6ccccb46024552ae1b3ee018
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-08 13:41:35 +0900

    백준 11720번

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,1 +1,7 @@
-print(Character(readLine()!).asciiValue!)
+let input = Int(readLine()!)!
+let n = Array(readLine()!)
+var total = 0
+for i in 0..<input {
+    total += Int(String(n[i]))!
+}
+print(total)
