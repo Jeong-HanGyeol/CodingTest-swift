6fef8238afe938b69545bcef4fb866ddc3fe8c48
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-09 23:25:28 +0900

    백준 1152번

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,9 +1,2 @@
-let t = Int(readLine()!)!
-for _ in 0..<t {
-    let input = readLine()!.split(separator: " ")
-    let R = Int(input[0])!
-    let S = Array(input[1])
-    var P = ""
-    S.map { P += String(repeating: String($0), count: R) }
-    print(P)
-}
+
+print(readLine()!.split(separator: " ").count)
