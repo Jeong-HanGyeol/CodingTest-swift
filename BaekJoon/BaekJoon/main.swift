3ea7c945b54e028ee9704fa55b7d3fed05051b58
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-09 23:43:38 +0900

    백준 2908번

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,3 +1,8 @@
-while let line = readLine() {
-    print(line)
+let input = readLine()!.split(separator: " ")
+let a = Int(String(input[0].reversed()))!
+let b = Int(String(input[1].reversed()))!
+if a > b {
+    print(a)
+} else {
+    print(b)
 }
