f3e346a04268be79da14b9e6e3fdc860d1e05943
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-10 11:10:05 +0900

    백준 25083번

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,14 +1,8 @@
-let dial = ["ABC","DEF","GHI","JKL","MNO","PQRS","TUV","WXYZ"]
-let word = Array(readLine()!.map {$0})
-var result = 0
-for i in 0..<dial.count {
-    let arr = dial[i].map { $0 }
-    for k in 0..<word.count {
-        for j in 0..<arr.count {
-            if word[k] == arr[j] {
-                result += (i + 3)
-            }
-        }
-    }
-}
-print(result)
+
+print(#"         ,r'"7"#)
+print(#"r`-_   ,'  ,/"#)
+print(#" \. ". L_r'"#)
+print(#"   `~\/"#)
+print(#"      |"#)
+print(#"      |"#)
+            
