c07b8fcb9073251317a5b859f75fef5918a0c966
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-10 11:32:48 +0900

    백준 3003번

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,8 +1,7 @@
-
-print(#"         ,r'"7"#)
-print(#"r`-_   ,'  ,/"#)
-print(#" \. ". L_r'"#)
-print(#"   `~\/"#)
-print(#"      |"#)
-print(#"      |"#)
-            
+let find = readLine()!.split(separator: " ").map {Int($0)!}
+let val = [1, 1, 2, 2, 2, 8]
+var result = [0, 0, 0, 0, 0, 0]
+for i in 0..<6 {
+    result[i] = val[i] - find[i]
+}
+print(result.map { String($0) }.joined(separator: " "))
