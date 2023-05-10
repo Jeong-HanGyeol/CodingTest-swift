231bc15338627688667b9fa035df8caa4612ba70
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-10 11:02:06 +0900

    백준 5622번

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,8 +1,14 @@
-let input = readLine()!.split(separator: " ")
-let a = Int(String(input[0].reversed()))!
-let b = Int(String(input[1].reversed()))!
-if a > b {
-    print(a)
-} else {
-    print(b)
+let dial = ["ABC","DEF","GHI","JKL","MNO","PQRS","TUV","WXYZ"]
+let word = Array(readLine()!.map {$0})
+var result = 0
+for i in 0..<dial.count {
+    let arr = dial[i].map { $0 }
+    for k in 0..<word.count {
+        for j in 0..<arr.count {
+            if word[k] == arr[j] {
+                result += (i + 3)
+            }
+        }
+    }
 }
+print(result)
