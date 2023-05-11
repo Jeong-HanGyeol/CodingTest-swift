b354f6bfadeba6a24aa9a9fae59cb0eebf4c4d05
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-11 13:09:23 +0900

    백준 2941번

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,18 +1,9 @@
 import Foundation
-let c = Int(readLine()!)!
-for _ in 0..<c {
-    var arr = readLine()!.split(separator: " ").map {Double($0)!}
-    let n = arr[0]
-    arr.remove(at: 0)
-    var t = 0.0
-    var r = 0.0
-    for i in arr {
-        t += i
-    }
-    for i in arr {
-        if t / n < i {
-            r += 1
-        }
-    }
-    print("\(String(format: "%.3f", round(r/n*100*1000)/1000))%")
+var input = readLine()!
+let croatia: [String] = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+
+for ch in croatia {
+    input = input.replacingOccurrences(of: ch, with: "b")
 }
+var count: Int = input.count
+print(count)
