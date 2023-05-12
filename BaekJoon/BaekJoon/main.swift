bf6c576f349fb63b630275275fab3017373a3b99
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-12 10:59:21 +0900

    25206번

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,20 +1,34 @@
-let n = Int(readLine()!)!
-var r = 0
-for _ in 0..<n {
-    let word = Array(readLine()!)
-    var ch = [Character]()
-    for i in 0..<word.count {
-        if ch.contains(word[i]) {
-            if word[i] == word[i-1] {
-
-            } else {
-                r += 1
-                break
-            }
-        } else {
-            ch.append(word[i])
-        }
+import Foundation
+var majorSubject = 0.0
+var total = 0.0
+for _ in 0..<20 {
+    let input = readLine()!.split(separator: " ")
+    let score = Double(input[1])!
+    let credit = input[2]
+    var subjectRating = 0.0
+    if credit == "A+" {
+        subjectRating = 4.5
+    } else if credit == "A0" {
+        subjectRating = 4.0
+    } else if credit == "B+" {
+        subjectRating = 3.5
+    } else if credit == "B0" {
+        subjectRating = 3.0
+    } else if credit == "C+" {
+        subjectRating = 2.5
+    } else if credit == "C0" {
+        subjectRating = 2.0
+    } else if credit == "D+" {
+        subjectRating = 1.5
+    } else if credit == "D0" {
+        subjectRating = 1.0
+    } else {
+        subjectRating = 0
+    }
+    if credit != "P" {
+        total += score
+        majorSubject += score * subjectRating
     }
 }
-print(n-r)
+print(String(format: "%.6f", majorSubject / total))
 
