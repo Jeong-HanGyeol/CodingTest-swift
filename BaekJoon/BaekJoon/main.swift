9e2e7b695e32f144c8cde844c5b77d50c026be1f
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-12 11:50:49 +0900

    백준 2738번

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,34 +1,20 @@
-import Foundation
-var majorSubject = 0.0
-var total = 0.0
-for _ in 0..<20 {
-    let input = readLine()!.split(separator: " ")
-    let score = Double(input[1])!
-    let credit = input[2]
-    var subjectRating = 0.0
-    if credit == "A+" {
-        subjectRating = 4.5
-    } else if credit == "A0" {
-        subjectRating = 4.0
-    } else if credit == "B+" {
-        subjectRating = 3.5
-    } else if credit == "B0" {
-        subjectRating = 3.0
-    } else if credit == "C+" {
-        subjectRating = 2.5
-    } else if credit == "C0" {
-        subjectRating = 2.0
-    } else if credit == "D+" {
-        subjectRating = 1.5
-    } else if credit == "D0" {
-        subjectRating = 1.0
-    } else {
-        subjectRating = 0
-    }
-    if credit != "P" {
-        total += score
-        majorSubject += score * subjectRating
+let input = readLine()!.split(separator: " ")
+let n = Int(input[0])!, m = Int(input[1])!
+var matrixA: [[Int]] = []
+var matrixB: [[Int]] = []
+var answer = [[Int]](repeating: [Int](repeating: 0, count: m), count: n)
+for _ in 0..<n {
+    matrixA.append(readLine()!.split(separator: " ").map { Int($0)! })
+}
+for _ in 0..<n {
+    matrixB.append(readLine()!.split(separator: " ").map { Int($0)! })
+}
+for y in 0..<n {
+    for x in 0..<m {
+        answer[y][x] = matrixA[y][x] + matrixB[y][x]
     }
 }
-print(String(format: "%.6f", majorSubject / total))
-
+for y in 0..<n {
+    answer[y].forEach { print($0, terminator: " ") }
+    print()
+}
