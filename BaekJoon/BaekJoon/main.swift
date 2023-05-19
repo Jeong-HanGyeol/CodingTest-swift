72e2c05f71ca41a5dc2163253d221b2168d9db74
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-05-19 11:32:58 +0900

    백준 2720번

--- BaekJoon/BaekJoon/main.swift
+++ BaekJoon/BaekJoon/main.swift
@@ -1,6 +1,11 @@
-let input = readLine()!.split(separator: " ")
+let t = Int(readLine()!)!
 
-let decimal = Int(input[0])!
-let num = Int(input[1])!
-
-print(String(decimal, radix: num).uppercased())
+for i in 0..<t {
+    let c = Int(readLine()!)!
+    
+    let q = c / 25
+    let d = (c - (q * 25)) / 10
+    let n = (c - (q * 25)-(d * 10)) / 5
+    let p = (c - (q * 25)-(d * 10)-(n * 5)) / 1
+    print("\(q) \(d) \(n) \(p)")
+}
